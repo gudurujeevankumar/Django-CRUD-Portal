8e7331a5d6811ef66a8531662b3108e1565c29af
Jeevan Kumar Guduru <jeevankumarguduru3@gmail.com>
2026-05-07 15:35:21 +0530

    Mobile layout was properly adjusted

--- student/views.py
+++ student/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
-from .models import Sdetails
+from . models import Sdetails
 from django.db import IntegrityError
 from django.contrib import messages
 from django.contrib.auth.decorators import login_required
